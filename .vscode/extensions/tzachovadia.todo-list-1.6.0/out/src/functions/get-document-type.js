a6af7cc708805f4d39bdac329d25a7a06189d217
sdeanchorena <106044196+sdeanchorena@users.noreply.github.com>
2022-06-12 20:32:40 -0300

    Delete .vscode directory

--- .vscode/extensions/tzachovadia.todo-list-1.6.0/out/src/functions/get-document-type.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getDocumentType(fsPath) {
-    return (/\.([\w]+)$/.exec(fsPath) || [null]).pop();
-}
-exports.getDocumentType = getDocumentType;
-//# sourceMappingURL=get-document-type.js.map
--- .vscode/extensions/tzachovadia.todo-list-1.6.0/out/src/functions/register-command.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vscode = require("vscode");
-function registerCommand(context, name, handler) {
-    context.subscriptions.push(vscode.commands.registerCommand(name, handler));
-}
-exports.registerCommand = registerCommand;
-//# sourceMappingURL=register-command.js.map
